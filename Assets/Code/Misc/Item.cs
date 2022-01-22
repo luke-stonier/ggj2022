9d9bd809cc2ddc5d9be974f2f588a60288ade589
lukestonier <lukestonier@hotmail.com>
2022-01-22 13:42:25 +0000

    simple inv

---
+++ Assets/Code/Misc/Item.cs
@@ -0,0 +1,34 @@
+﻿class Item : IItem
+{
+    private int _id = -1;
+    private int _count = 0;
+
+    public int count()
+    {
+        return _count;
+    }
+
+    public int id()
+    {
+        return _id;
+    }
+
+    public void increaseCount(int count)
+    {
+        _count += count;
+    }
+
+    public void decreaseCount(int count)
+    {
+        _count -= count;
+    }
+
+    public IItem droppedItemWithCount(int count)
+    {
+        return new Item
+        {
+            _id = _id,
+            _count = count
+        };
+    }
+}
